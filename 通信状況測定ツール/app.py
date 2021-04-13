8dd3c4f391446efc2575bd1b175cc5bf5212ddb4
光岡知洋 <mitsuokatomohiro@MacPro.local>
2021-04-13 20:15:50 +0900

    final

---
+++ 通信状況測定ツール/app.py
@@ -0,0 +1,22 @@
+import psutil
+
+
+def main():
+    # メモリ容量を取得
+    mem = psutil.virtual_memory() 
+    print(mem.total)
+    
+    print range(3)
+    # メモリ使用量を取得 
+    print(mem.used)
+
+    # メモリ空き容量を取得 
+    print(mem.available)
+
+    # CPU使用率を取得 
+    cpu = psutil.cpu_percent(interval=1)
+    print(cpu)
+
+# プログラム7｜mainを呼び出す
+if __name__ == "__main__":
+    main()
